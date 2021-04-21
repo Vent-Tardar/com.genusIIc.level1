0537e1396a463789002abc8aa196ad7f9736f191
Vent-Tardar <79703422+Vent-Tardar@users.noreply.github.com>
2021-04-21 15:27:22 +0300

    Add files via upload

--- Main.java
+++ Main.java
@@ -1,8 +1,5 @@
 package com.genusIIc.level1;
 
-import java.io.IOException;
-import java.util.Scanner;
-
 public class Main {
     public static void main(String[] args) throws Exception {
         ComparisonDoc cd = new ComparisonDoc();
