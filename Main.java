d76390c3562a19283ea9b9419a3040dbe3c9227d
Vent-Tardar <79703422+Vent-Tardar@users.noreply.github.com>
2021-04-22 11:42:26 +0300

    Add files via upload

--- Main.java
+++ Main.java
@@ -2,7 +2,22 @@ package com.genusIIc.level1;
 
 public class Main {
     public static void main(String[] args) throws Exception {
-        ComparisonDoc cd = new ComparisonDoc();
-        cd.compare(args[0], args[1]);
+        switch (args.length){
+            case 0: System.err.println("You didn't enter any parameters");
+            break;
+            case 1: System.err.println("You have entered one parameter, enter another parameter");
+            break;
+            case 3:
+            case 4:
+                System.err.printf("The program should have 2 parameters args[], you entered only %d\n", args.length);
+                for(int i = 0; i < args.length; i++){
+                    System.err.printf("Your parameter args[%d]: %s\n", i, args[i]);
+                }
+                break;
+            default:
+                ComparisonDoc cd = new ComparisonDoc();
+                cd.compare(args[0], args[1]);
+                break;
+        }
     }
 }
